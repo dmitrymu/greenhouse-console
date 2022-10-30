0f3c50973950e941413418bb5994181de2da9aa9
Dmitry Mukhortov <d.v.mukhortov@gmail.com>
2022-10-30 11:56:14 -0700

    Add python installer

--- init.sh
+++ init.sh
@@ -1,3 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 
-./.venv/bin/activate
+if [ ! -d .venv ]; then
+    mkdir .venv
+    python -m venv .venv
+fi
+
+source .venv/bin/activate
+
+pip install -U wheel
+pip install -U PyQt6
+pip install -U paho-mqtt
+pip install -U PyInstaller pyinstaller-hooks-contrib    
